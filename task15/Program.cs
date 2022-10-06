e7656619af8cd7b7c410f18d3bb55da3deb48b2e
HemlosT <114144823+HemlosT@users.noreply.github.com>
2022-10-06 22:31:46 +0900

    homework completed

---
+++ task15/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+void dayInTheWeek (int dayNumber)
+{
+    if(dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("да");
+    }
+
+    else if(dayNumber > 7 || dayNumber < 1)
+    {
+        Console.WriteLine("Такого дня недели нет");
+    }
+
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+
+dayInTheWeek(6);
